e8ee961683436ec93d0e1fd86238eb161b51dcdf
Chinmay Agrawal <thechinmayagrawal@gmail.com>
2025-07-07 06:11:16 +0530

    making app

---
+++ sw.js
@@ -0,0 +1,33 @@
+const cacheName = 'kanha-library-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/admin.html',
+  '/recent-bookings.html',
+  '/styles.css',
+  '/login.css',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/booking.html',
+  '/booking.js',
+  '/signup.html',
+  '/signup.css',
+  // add other CSS/JS/image files if needed
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
