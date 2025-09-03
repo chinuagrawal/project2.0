1c350e35a7e3e6de209088826f9446588498de11
Chinmay Agrawal <thechinmayagrawal@gmail.com>
2025-09-03 12:15:09 +0530

    fixing asking payment agian

--- Backend2.0/routes/webhook.js
+++ Backend2.0/routes/webhook.js
@@ -8,7 +8,8 @@ router.post('/phonepe/webhook', async (req, res) => {
   try {
     console.log('📥 PhonePe Callback Received:', req.body);
 
-    const payload = req.body.payload;
+    const payload = req.body.payload || req.body;
+
     if (!payload) {
       console.error("❌ Invalid webhook format");
       return res.status(200).send("OK");
